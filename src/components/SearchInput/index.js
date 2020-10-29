7f7aea424854f302ca7a640ed43f466eba432b84
oscaraag <oscaraag1493@gmail.com>
2020-10-29 00:30:22 -0400

    Checking styles 🚀

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
+// import search from '../../assets/search-solid.svg'
+import { SearchIcon } from '../SearchIcon'
 
 export const SearchInput = ({ query, setQuery }) => {
 	return (
 		<>
 			<nav className='header__input'>
-				<span>Buscar</span>
 				<input
+					placeholder='Buscar'
 					value={query}
 					type='text'
 					onChange={e => {
 						setQuery(e.target.value)
 					}}
 				/>
+				<SearchIcon />
 			</nav>
 		</>
 	)
